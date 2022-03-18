960611cc09517b46623264c65f5ded74c18f2ba0
eneox <96574231+eneox@users.noreply.github.com>
2022-03-18 17:02:28 +0300

    w5500_project

---
+++ ethernet_project_01/Debug/Core/Src/w5500/loopback.d
@@ -0,0 +1,14 @@
+Core/Src/w5500/loopback.o: ../Core/Src/w5500/loopback.c \
+ ../Core/Src/w5500/loopback.h ../Core/Src/w5500/socket.h \
+ ../Core/Src/w5500/wizchip_conf.h ../Core/Src/w5500/W5500/w5500.h \
+ ../Core/Src/w5500/wizchip_conf.h
+
+../Core/Src/w5500/loopback.h:
+
+../Core/Src/w5500/socket.h:
+
+../Core/Src/w5500/wizchip_conf.h:
+
+../Core/Src/w5500/W5500/w5500.h:
+
+../Core/Src/w5500/wizchip_conf.h:
